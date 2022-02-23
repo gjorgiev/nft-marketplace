d851bade483fbf4943aa03d285ffd41cb78951f5
gjorgiev <gjorgiev991@gmail.com>
2022-02-23 12:25:00 +0100

    deployment commit

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,6 @@ export default function Home() {
     await transaction.wait()
     loadNFTs()
   }
-
   if (loadingState === 'loaded' && !nfts.length) return (<h1 className='px-20 py-10 text-3xl'>No items in marketplace</h1>)
 
   return (
