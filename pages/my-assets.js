6ed5ac07cf8d636aace61d63545cb9fe61e3b9ff
gjorgiev <gjorgiev991@gmail.com>
2022-02-23 13:04:59 +0100

    imported Image

--- pages/my-assets.js
+++ pages/my-assets.js
@@ -2,6 +2,7 @@ import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from 'web3modal'
+import Image from 'next/image'
 
 import {
     nftmarketaddress, nftaddress
